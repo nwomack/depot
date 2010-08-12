1a37774630f2a4ee91aa0f76f1b5c3e0c13c8f75
Nathan Womack <nwomack@gmail.com>
2010-08-12 18:55:19 +0800

    task_b complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Depot::Application.routes.draw do
-  resources :products
+  #get "store/index"
+
+  #resources :products
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,5 +58,5 @@ Depot::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
