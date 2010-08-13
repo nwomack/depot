ae0832c50675108efdf800e8e2a946210fada313
Nathan Womack <nwomack@gmail.com>
2010-08-13 16:22:40 +0800

    Task C Complete

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,33 @@
 class StoreController < ApplicationController
+  def add_to_cart
+    begin
+      product = Product.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error("Attempt to access invalid product #{params[:id]}")
+      redirect_to_index("Invalid Product")
+    else
+      @cart = find_cart
+      @cart.add_product(product)
+    end
+  end
+  
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index("Your cart is currently empty")
+  end
+  
   def index
     @products = Product.find_products_for_sale
   end
 
+private
+
+  def redirect_to_index(msg)
+    flash[:notice] = msg
+    redirect_to :action => :index
+  end
+
+  def find_cart
+    session[:cart] ||= Cart.new
+  end
 end
